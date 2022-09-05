6c3015dbf539d599358d68fbeeeb0a35c1440c51
vnknt <kanatvn@gmail.com>
2022-09-06 00:54:49 +0300

    Add functions for autowired and component decorators

--- ioc/decorators/ioc/Autowired.ts
+++ ioc/decorators/ioc/Autowired.ts
@@ -1,4 +1,30 @@
-export default function Autowired(name?:string|undefined){
+import 'reflect-metadata'
+import DependencyObserver from '../../containers/dependency/DependencyObserver';
+import DependencyInjector from '../../containers/DependencyInjector';
 
+export default function Autowired(name?:string|undefined){
+    return (target:any, key:string): void => {
+        
+        var t = Reflect.getMetadata("design:type", target, key);
+        var beanName;
+        if(t==undefined){
+            beanName=name
+        }else{
+            beanName=t.name
+        }
+        let dependencyObserver =new DependencyObserver(beanName.trim(),target,key,(injectedObject:any,t:any,k:any)=>{
+
+            Object.defineProperty(t, k, {
+                get: function() {
+                    return injectedObject;
+                },
+                set: function(value) {
+                }
+            })
+        });
+        DependencyInjector.getInstance().register(dependencyObserver)
     
+      };
+
+
 }
--- ioc/decorators/ioc/Component.ts
+++ ioc/decorators/ioc/Component.ts
@@ -1,3 +1,12 @@
-export default function Component(){
+import { RegistrationTypes } from "../../constants/RegistrationTypes"
+import ContainerProvider from "../../containers/ContainerProvider"
+import DependencyInjector from "../../containers/DependencyInjector"
+import ContainerObject from "../../types/ContainerObject"
 
+export default function Component(){
+    return function (ctr:Function){
+        let containerObject = new ContainerObject(ctr.name,ctr)
+        ContainerProvider.getContainer(RegistrationTypes.SINGLETON).register(containerObject)
+        DependencyInjector.getInstance().insertObject(containerObject.getName())
+    }
 }
