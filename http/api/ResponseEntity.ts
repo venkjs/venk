ff03132dba86abc3e801e55cde6373e5619c0cde
vnknt <kanatvn@gmail.com>
2022-10-11 11:55:44 +0300

    Add middleware support for endpoints and fix async function bug

--- http/api/ResponseEntity.ts
+++ http/api/ResponseEntity.ts
@@ -5,7 +5,7 @@ export class ResponseEntity<T>{
     
     private constructor(status:number){
         this.status=status
-        Object.setPrototypeOf(this,ResponseEntity)
+        Object.setPrototypeOf(ResponseEntity.prototype,ResponseEntity)
     }
 
     public static status<T>(status:number):ResponseEntity<T>{
